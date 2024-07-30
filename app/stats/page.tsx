2a96c652ade5bf883b426b78cbf733097939a4ee
Mks-1234-dev <seemasatya2@gmail.com>
2024-07-30 12:20:06 -0500

    19

--- app/stats/page.tsx
+++ app/stats/page.tsx
@@ -26,7 +26,38 @@ const Graph: React.FC<GraphProps> = ({ data, label, lineColor, fillColor }) => {
 	const stats = calculateStats(data);
 
 	return (
-	
+		<div className={styles.statsContainer}>
+			<div className={styles.graphContainer}>
+				<Line
+					data={{
+						labels: Array.from({ length: data.length }, (_, i) => (i + 1).toString()),
+						datasets: [{
+							label,
+							data,
+							fill: true,
+							borderColor: lineColor,
+							backgroundColor: fillColor
+						}],
+					}}
+				/>
+			</div>
+			<div className={styles.statsInfo}>
+				<div className={styles.stat}>
+					<p>Average: </p>
+					<p style={{ color: lineColor }}>{stats.average.toFixed(2)}</p>
+				</div>
+				<br />
+				<div className={styles.stat}>
+					<p>Standard deviation: </p>
+					<p style={{ color: lineColor }}>{stats.standardDeviation.toFixed(2)}</p>
+				</div>
+				<br />
+				<div className={styles.stat}>
+					<p>Max: </p>
+					<p style={{ color: lineColor }}>{stats.max.toFixed(2)}</p>
+				</div>
+			</div>
+		</div>
 	);
 };
 
